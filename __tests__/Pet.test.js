ebf596aa72bebe994158a19a1c22b9ad3e6c793c
gergana <gergana_ruse@yahoo.com>
2018-03-04 18:57:17 +0000

    updated tests

--- __tests__/Pet.test.js
+++ __tests__/Pet.test.js
@@ -20,7 +20,7 @@ describe("constructor", () => {
 		fido.growUp();
 		expect(fido.age).toEqual(1);
 		expect(fido.hunger).toEqual(5);
-		expect(fido.fitness).toEqual(7)
+		expect(fido.fitness).toEqual(7);
 	});
 
 	
@@ -39,8 +39,40 @@ describe("constructor", () => {
 
 	});	
 
-	it("")
+	it("returns iam hungry when hunger lever is 5 or above", () => {
+		const fido = new Pet("Fido");
+		fido.hunger = 7;
+		expect(fido.checkUp()).toBe("I am hungry");
+
+	});
+	it("returns true when hunger is under 10, fitness above 0 and age under 30", () => {
+		const fido = new Pet("Fido");
+		fido.age = 29;
+		fido.fitness = 1;
+		fido.hunger = 9;
+		expect(fido.isAlive).toBeTruthy();
+		
+		
+	});
+	it("returns false if hunger is over 10 or fitness less than 0 or age above 30", () => {
+		const fido = new Pet("Fido");
+		fido.age = 31;
+		fido.fitness = 0;
+		fido.hunger = 10;
+		expect(fido.isAlive).toBeFalsy();
+		
+		
+	});
     
-    
+	xit("throws an error if the pet is not alive", () => {
+		const fido = new Pet("Fido");
+  
+		fido.age = 13;
+  
+		expect(fido.feed).toThrow("Your pet is no longer alive :(");
+	});
 });
+  
+  
+
   
