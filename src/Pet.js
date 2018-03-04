dd53a08cfdc38bb6c0ac7acd31be19d4b8bba8fe
gergana <gergana_ruse@yahoo.com>
2018-03-04 18:52:22 +0000

    updated version

--- src/Pet.js
+++ src/Pet.js
@@ -3,14 +3,23 @@ function Pet(name) {
 	this.age = 0;
 	this.hunger = 0;
 	this.fitness = 10;
+}
+
+Pet.prototype = {
+	get isAlive() {
+	  return this.age < 30 && this.hunger < 10 && this.fitness > 0;
+	} 
 };
+  
 
 Pet.prototype.growUp = function() {
+		
 	this.age += 1;
 	this.hunger += 5;
 	this.fitness -= 3;
 };
 Pet.prototype.walk = function() {
+		  
 	if ((this.fitness + 4) <= 10) {
 		this.fitness += 4;
 	} else {
@@ -18,7 +27,9 @@ Pet.prototype.walk = function() {
 	}
 
 };
+
 Pet.prototype.feed = function() {
+		  
 	if ((this.hunger - 3) >= 0) {
 		this.hunger -= 3;
 	} else {
@@ -27,18 +38,20 @@ Pet.prototype.feed = function() {
 
 };
 Pet.prototype.checkUp = function() {
-	if(Pet.fitness <= 3) {
-		return "I need a walk";
-	}
-	if(Pet.hunger >= 5) {
-		return "I am hungry";
-	}
-	if(Pet.fitness <= 3 && Pet.hunger >= 5) {
+		
+	if((this.fitness <= 3) && (Pet.hunger >= 5)) {
 		return "I am hungry AND I need a walk";
 	}
-	if (!(Pet.fitness <= 3) && !(Pet.hunger >= 5)) {
+	if (!(this.fitness >= 3) && !(this.hunger >= 5)) {
 		return "I feel great!";
 	}
+	if(this.fitness <= 3) {
+		return "I need a walk";
+	}
+	if(this.hunger >= 5) {
+		return "I am hungry";
+	}
+	
 };
 const fido = new Pet("Fido");
 fido.growUp();
